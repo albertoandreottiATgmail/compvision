ae53c40087bd9cdcf474915fe9d0d0d7acd085d4
Alberto <albertoandreotti@gmail.com>
2016-10-01 17:29:19 -0300

    added script for running problems 1 & 2

---
+++ pr1/run_p1.sh
@@ -0,0 +1,7 @@
+#exercise 1
+workon cv
+rm *.pk
+for i in `seq 1 100`;
+    do
+        python lab1.py
+    done  
---
+++ pr1/run_p2.sh
@@ -0,0 +1,5 @@
+#exercise 2
+for i in `seq 10 2 200`;
+    do
+        python lab1.py -nc $i
+    done  
