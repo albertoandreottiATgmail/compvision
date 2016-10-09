d066c926df3a1962b1cf1fdfe82c2c16efa74487
Alberto <albertoandreotti@gmail.com>
2016-10-09 14:51:17 -0300

    script for running ex2

--- pr1/run_p2.sh
+++ pr1/run_p2.sh
@@ -1,5 +1,10 @@
 #exercise 2
 for i in `seq 10 2 200`;
     do
+        rm  cache/*.dat
+        rm  cache/*.feat
+        cd cache
+        rm **/*.bovw
+        cd ..
         python lab1.py -nc $i
     done  
